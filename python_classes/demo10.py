c3b6c51fd9166c1ecce716922a296804aa8d50b5
Naveen Kumar Nakshatram <mailmenaveenkumar@gmail.com>
2026-06-11 17:56:07 +0100

    2

---
+++ python_classes/demo10.py
@@ -0,0 +1,13 @@
+
+nos = [30,23,4,32,55,2,55,666,32,6642,77]
+
+print(sum(nos))
+
+
+import calendar
+
+print(calendar.calendar(2026))
+
+print(calendar.calendar(2026,6))
+
+
---
+++ python_classes/demo9.py
@@ -0,0 +1,6 @@
+#  Consider a is 5 and b is 3, store a in b print a and b?
+
+a = 5
+b = 3
+b = a
+print(a,b)
